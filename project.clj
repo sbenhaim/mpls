2354a739e5bd3f018c1449ab7a533db07e1e3993
Selah Ben-Haim <selahb@gmail.com>
2015-03-24 22:48:21 -0500

    Renamed to mpls. Narrowed scope.

--- project.clj
+++ project.clj
@@ -1,15 +1,13 @@
-(defproject com.anthropology-music/max-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject mpls "0.1.0-SNAPSHOT"
+  :description "Live code Max and Max4Live with Clojure."
+  :url "https://github.com/sbenhaim/mpls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.cycling74/max "6.1.0"]
-                 [cider/cider-nrepl "0.8.1"]
-                 [org.clojure/core.match "0.2.1"]
-                 [org.clojure/tools.nrepl "0.2.5"]
-                 [overtone "0.9.1"]]
-  :aot [com.anthropology-music.clj]
-  :jar-name "max-clj-small.jar"
-  :uberjar-name "max-clj.jar")
+                 [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/tools.nrepl "0.2.9"]]
+  :aot [mpls]
+  :jar-name "mpls-small.jar"
+  :uberjar-name "mpls.jar")
--- src/mpls.clj
+++ src/mpls.clj
@@ -1,6 +1,6 @@
-(ns com.anthropology-music.clj
+(ns mpls
   (:gen-class
-   :name clj
+   :name mpls
    :init init
    :post-init post-init
    :constructors {[] []
@@ -10,13 +10,11 @@
    :extends com.cycling74.max.MaxObject)
   (:require [clojure.tools.nrepl.server :as server]
             [clojure.string :as s]
-            [clojure.core.async :refer [go alts!! timeout chan >!]]
             [clojure.core.match :refer [match]]
-            [cider.nrepl :refer [cider-nrepl-handler]]
-            [overtone.music.pitch :refer :all])
+            [cider.nrepl :refer [cider-nrepl-handler]])
   (:import [com.cycling74.max Atom]))
 
-(declare this max-box max-patcher max-window port)
+(declare mpls max-box max-patcher max-window port)
 
 (def user-ns (atom nil))
 
@@ -26,10 +24,8 @@
 (defn call-user-fn [sym & args]
   (if-let [f (resolve (symbol (str @user-ns "/" sym)))]
     (apply f args)
-    (println (str @user-ns "/" sym) " unimplemented.\n\nCall (hello-max-clj!)")))
+    (println (str @user-ns "/" sym) " unimplemented.")))
 
-
-(def msg-chan (chan))
 
 (defn matom [arg]
   (Atom/newAtom arg))
@@ -66,7 +62,7 @@
   ([this _ in out] (-post-init this in out))
   ([this in out] 
      (.declareIO this in out)
-     (defonce this this)
+     (defonce mpls this)
      (defonce max-box (.getMaxBox this))
      (defonce max-patcher (.getPatcher max-box))
      (defonce max-window (.getWindow max-patcher))))
@@ -99,60 +95,5 @@
 (defn msend [object message & args]
   (.send object message (apply matoms args)))
 
-(defn lsend [object k message & args]
-  (apply msend (object k) message args))
-
-(defn lchildren [o]
-  (msend (o :path) "getchildren"))
-
 (defn mnew [name x y & args]
   (.newDefault max-patcher x y name (apply matoms args)))
-
-(defn lnew [& path]
-  (let [lp (apply mnew "live.path" 200 100 path)
-        lo (mnew "live.object" 200 140)]
-    (.setName lp (str path " path"))
-    (.setName lo (str path " object"))
-    (.connect max-patcher lp 0 lo 1)
-    (.connect max-patcher lo 0 max-box 0)
-    (fn [msg & args]
-      (condp = msg
-        :path lp
-        :obj lo
-        "goto" (apply msend lp "goto" (mapcat #(s/split % #" ") args))
-        "getchildren" (msend lp "getchildren")
-        "get" (do
-                (apply msend lo msg args)
-                (let [[v c] (alts!! [msg-chan (timeout 100)])]
-                  v))
-        (do
-          (apply msend lo msg args))))))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
